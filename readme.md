382b8bde7cb400d29ac0b91cc5e6f078a2ba1181
Dyecon <25501036+dyecon@users.noreply.github.com>
2024-05-09 22:08:07 +0900

    Add all files

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Official Website of Just Shoot
+
+References:
+- [Responsive topnav](https://www.w3schools.com/howto/howto_js_topnav_responsive.asp)
+- [Fullscreen video](https://www.w3schools.com/howto/howto_css_fullscreen_video.asp)
+- [Accordion](https://www.w3schools.com/howto/howto_js_accordion.asp)
+- [Avatar Images](https://www.w3schools.com/howto/howto_css_image_avatar.asp)
+- [Fading Buttons](https://www.w3schools.com/howto/howto_css_fading_buttons.asp)
---
+++ script.js
@@ -0,0 +1,46 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+let slideIndex = 1;
+function showSlides(n) {
+    if(document.title=="Just Shoot")
+    {
+        let i;
+    let slides = document.getElementsByClassName("mySlides");
+    console.log(slides);
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+    }
+}
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
